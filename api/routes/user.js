e5e76d5171a571fd97623c93e1c81458f217e4e8
OrlandoL10 <lorlando127@gmail.com>
2018-08-08 02:41:24 -0500

    updated gitignore

--- api/routes/user.js
+++ api/routes/user.js
@@ -9,19 +9,19 @@ router.post('/signup', UserController.create_user)
 router.post("/login", UserController.login_user)
 
 
-router.get("", (req, res, next) => {
-	User.find().then(response => {
-		res.status(200).json({Users: response})
-	})
-})
+// router.get("", (req, res, next) => {
+// 	User.find().then(response => {
+// 		res.status(200).json({Users: response})
+// 	})
+// })
 
-router.delete("/:id", (req, res, next) => {
-	User.deleteOne({_id: req.params.id}).then(feedback => {
-		res.status(202).json({message: feedback})
-	}).catch(err => {
-		res.status(400).json({error: err})
-	})
-})
+// router.delete("/:id", (req, res, next) => {
+// 	User.deleteOne({_id: req.params.id}).then(feedback => {
+// 		res.status(202).json({message: feedback})
+// 	}).catch(err => {
+// 		res.status(400).json({error: err})
+// 	})
+// })
 
 
 module.exports = router;
